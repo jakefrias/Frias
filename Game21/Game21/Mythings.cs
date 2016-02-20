5f0c7c3c581a559ca7c62d76313ac4dd58c164b2
Amiel Jake E. Frias <jake.frias@yahoo.com>
2016-02-20 09:35:40 -0800

    Version 1

---
+++ Game21/Game21/Mythings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Game21
+{
+    class Mythings
+    {
+
+        public Texture2D Texture { get; set; }
+
+        public int Rows { get; set; }
+
+        public int Columns { get; set; }
+
+        private int currentFrame;
+
+        private int totalFrames;
+
+        private int timeSinceLastFrame = 0;
+
+        private int millisecondsPerFrame = 200;
+
+        public Mythings(Texture2D texture, int rows, int columns)
+        {
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            currentFrame = 0;
+            totalFrames = Rows * Columns;
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+            timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (timeSinceLastFrame > millisecondsPerFrame)
+            {
+                timeSinceLastFrame -= millisecondsPerFrame;
+                currentFrame++;
+                timeSinceLastFrame = 0;
+                if (currentFrame == totalFrames)
+                {
+                    currentFrame = 0;
+                }
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            int width = Texture.Width / Columns;
+            int height = Texture.Height / Rows;
+            int row = (int)((float)currentFrame / Columns);
+            int column = currentFrame % Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+
+        }
+
+    }
+}
